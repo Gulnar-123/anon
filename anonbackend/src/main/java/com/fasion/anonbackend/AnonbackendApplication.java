6d5c560bcbfbe0fb965c3b321f098b0f2fbca0d8
Gulnar-123 <gulnar6816@gmail.com>
2024-07-30 23:35:43 +0530

    Fashion

---
+++ anonbackend/src/main/java/com/fasion/anonbackend/AnonbackendApplication.java
@@ -0,0 +1,13 @@
+package com.fasion.anonbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnonbackendApplication.class, args);
+	}
+
+}
---
+++ anonbackend/src/test/java/com/fasion/anonbackend/AnonbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fasion.anonbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
